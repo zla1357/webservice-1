904cb0153a9b0d6083988bf1208df99a6c83a07e
khj <zla1275@gamil.com>
2022-02-04 23:08:05 +0900

    PostsApiController.java 삭제 메서드 deletemapping으로 수정

--- src/main/java/com/webservice/springboot/web/PostsApiController.java
+++ src/main/java/com/webservice/springboot/web/PostsApiController.java
@@ -32,7 +32,7 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
-    @GetMapping("api/v1/posts/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
