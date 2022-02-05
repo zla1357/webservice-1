7ae66a774118321e6161ff8d42a99c58969d1028
khj <zla1275@gamil.com>
2022-02-06 01:06:59 +0900

    spring security 관련 파일 추가

--- src/main/java/com/webservice/springboot/web/IndexController.java
+++ src/main/java/com/webservice/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.webservice.springboot.web;
 
+import com.webservice.springboot.config.auth.dto.SessionUser;
 import com.webservice.springboot.service.posts.PostsService;
 import com.webservice.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 /**
  * 화면을 요청하는 클라이언트의 요청을 처리하는 컨트롤러
@@ -16,10 +18,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
